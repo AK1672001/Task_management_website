5f2c90d2cbe5454eb86686aa737caf23b880afb4
Your Name <you@example.com>
2024-10-17 15:27:38 +0530

    complete backend

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,20 @@ const Authenticate=require("./Middleware/Authenticate")
 const dotenv=require("dotenv")
 dotenv.config();
 const server=express();
-
+const cors=require('cors')
 server.use(express.json())
 server.use(router)
 server.use(taskrouter);
 server.use(Authenticate)
+
+server.use(cors(
+    { 
+        origin:"http://localhost:3000",
+        credentials:true
+
+    }
+    
+))
 server.listen(process.env.PORT,()=>{
     console.log(`server is running ${process.env.PORT}`)
 })
