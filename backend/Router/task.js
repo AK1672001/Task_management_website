d71b225757c258d80807f5290290af57c79d7e03
Your Name <you@example.com>
2024-10-17 15:29:47 +0530

    updated task

--- backend/Router/task.js
+++ backend/Router/task.js
@@ -1,4 +1,4 @@
-const {taskpost,gettask,getonetask,taskupdate,singletask}=require("../Controller/task");
+const {taskpost,gettask,taskupdate,singletask,deletetask}=require("../Controller/task");
 const Authenticate=require("../Middleware/Authenticate")
 const express=require("express");
 
@@ -6,7 +6,8 @@ const taskrouter=express.Router();
 
 taskrouter.post("/addtask",taskpost);
 taskrouter.get("/gettask",gettask);
-taskrouter.get("/getonetask/:id",getonetask);
+// taskrouter.get("/getonetask/:id",getonetask);
 taskrouter.put("/taskupdate/:id",taskupdate);
 taskrouter.get("/singletask/:id",singletask);
+taskrouter.delete("/deletetask/:id",deletetask);
 module.exports=taskrouter;
