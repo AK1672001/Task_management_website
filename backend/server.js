ce74d14f226855d24d2038103773a920fe8549b6
Your Name <you@example.com>
2024-10-20 15:55:43 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./Router/router");
 const taskrouter = require("./Router/task");
-const path = require('path');
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 const server = express();
 const cors = require("cors");
 server.use(
-    cors({
-      origin: ["http://localhost:3000",'https://task-management-website-865l.onrender.com'],
-      credentials: true,
-    })
-  );
-  
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://task-management-website-865l.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 server.use(cookieParser());
 server.use(express.json());
+
 server.use(router);
 server.use(taskrouter);
-
 
 server.listen(process.env.PORT, () => {
   console.log(`server is running ${process.env.PORT}`);
@@ -34,8 +36,8 @@ mongoose
     console.log(err);
   });
 
-  const dirname=path.resolve();
-  server.use(express.static(path.join(dirname,'/frontend/build')));
-  server.get("*",(req,res)=>{
-      res.sendFile(path.join(dirname,"frontend",'build','index.html'))
-  })
+const dirname = path.resolve();
+server.use(express.static(path.join(dirname, "/frontend/build")));
+server.get("*", (req, res) => {
+  res.sendFile(path.join(dirname, "frontend", "build", "index.html"));
+});
